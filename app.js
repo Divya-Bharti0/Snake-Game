1cedd6db5f1900714246e0a0e179255e46113af1
Divya-Bharti0 <divyabharti2055@gmail.com>
2024-05-28 22:58:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,735 @@
+//let color="red";
+// traffic light system
+//if(color==="red"){
+   // console.log("stop!light color is red");
+//}
+//if(color==="yellow"){
+    //console.log("slow down!light color is yellow");
+//}
+//if(color==="green"){
+   // console.log("Go! light color is green")
+//}
+//let age=14;
+//if(age>=18){
+   // console.log("you can do vote");
+//}
+//else if(age<18){
+    //console.log("you cannot do vote");
+//}
+//let marks=75;
+//if(marks>=80){
+   // console.log("A++ ");
+
+//}
+//else if(marks>=65){
+    //console.log("A ");
+//}
+//else if(marks>=33){
+    //console.log("B");
+//}
+//else if(marks<33){
+   //console.log("F")
+//}
+//let month="january";
+//if(month=="january"){
+    //console.log("winter is here");
+//}
+//else if(month=="may"){
+    //console.log("summer is here");
+//let age=17;
+//if(age>=18){
+//console.log("you can vote");
+//}
+//else{
+   // console.log("you cannot vote");
+//}
+
+//let color="white";
+//if (color==="red"){
+   // console.log("stop");
+//}
+//else if(color==="yellow"){
+ // console.log("slow down");
+//}else if(color==="green"){
+   // console.log("go");
+//}else{
+   // console.log("traffic light is broken");
+//let marks=45;
+//if(marks>=33){
+   //console.log("you will be pass");
+//}
+//if(marks>=80){
+   // console.log("Grade:O");
+//}
+//else{
+    //console.log("Grade:A");
+//}`else`
+  //  console.log("Better Luck Next Time");
+//logical operatotrs
+//let marks=23;
+//if((marks>33 && marks<=80)|| !false){
+ //   console.log("pass");//pass
+//}
+//let str="apple";
+//if(str[0]==="a"&& str.length>9){
+  // console.log("good string");
+//
+//else{
+  // console.log("Not a good string");
+//}
+//let num="12";
+//if((num%3===0)&&(num+3==15)||(num-1==11)){
+  // console.log("safe");
+//}
+//else{
+   //console.log("unsafe");
+//}
+//if(true){
+   //console.log("it is true value");
+//}
+//else{
+   //console.log("it is false value");
+//}
+//let string="";
+//if(string){
+ //  console.log("string is not empty");
+//}
+//else{
+  // console.log("string is empty");
+//}
+//let num=0;
+//if(num){
+   //console.log("num is not equal to 0");
+//}
+//else{
+  // console.log("num is equal to zero");
+//}
+//let color="red";
+//switch(color){
+  // case "red":
+    //  console.log("stop!");
+     // break;
+    //  case "yellow":
+        // console.log("slow down!");
+         //break;
+       //  case "green":
+         //   console.log("go!");
+          //  break;
+//}
+//let day=1;
+//switch(day){
+  // case 1:
+      //console.log("monday");
+   
+     // case 2:
+         //console.log("tuesday");
+         
+        // case 3:
+           // console.log("wednesday");
+            
+          //  case 4:
+              //console.log("thursday");
+               
+              //case 5:
+                 // console.log("friday");
+                  
+                  //case 6:
+                    // console.log("saturday");
+                  
+                     //case 7:
+                       // console.log("sunday");
+                       // break;
+                       //default:
+                      //  console.log("wrong day!");
+//}
+
+//console.log("this is a simple log");
+//console.error("this is simple error msg");
+//console.warn("this is warning msg");
+//let firstName = prompt ("enter your name:");
+//console.log(firstName);
+//let msg=" hello "
+
+//for(let i=1;i<=9;i++){
+//console.log(i);
+//}
+//for(let i=1; i<=15; i+2){
+//console.log(i);
+//}
+//for(let i=1; i<=15;  i+1){
+   //console.log(i);
+//}
+
+//console.log("backwrds");
+//for(let i=15; i>=1; i=i-1){
+
+//  console.log(i);
+ // }
+ // for(let i=2; i<=10; i=i+2){
+ //  console.log(i);
+ // }
+//  console.log("backwards");
+ // for(let i=10; i>=2; i=i-2){
+  // console.log(i);
+ // }
+// for(let i=1; ; i++){
+//console.log(i);
+ //}
+ //for(let i=5; i<=50;i=i+5){
+  // console.log(i);
+ //}
+ //for(let i=10; i<=50; i=i+1){
+//console.log(i);
+ //}
+ //console.log("backwards");
+// for(let i=50; i>=10; i=i-1){
+   //console.log(i);
+ //}
+// let color="red";
+// traffic light system
+//if(color==="red"){
+//console.log("stop!light color is red");
+//}
+//if(color==="yellow"){
+ //  console.log("slow down light color is yellow");
+//}
+//if(color==="green"){
+  // console.log("go fast light color is green ");
+//}
+//let string="";
+//if(string){
+  // console.log("string is not empty");
+//}
+//// console.log("string is empty");
+//}
+//let age=14;
+//if(age<=18){
+  // console.log("you can vote");
+//}
+//else{
+//   console.log("you cannot vote");
+//}
+//let day=1;
+//switch (day){
+   //case 1:
+      //console.log("monday");
+     // case 2:
+        // console.log("tuseday");
+        // case 3:
+           // console.log("wednesday");
+          //  case 4:
+              //console.log("thursday");
+              // case 5:
+               //  console.log("friday");
+               // case 6:
+                 //console.log("saturday");
+                 // case 7:
+                  // console.log("sunday");
+
+//}
+//console.log("it is simple console log");
+//for(let i=1; i<=3; i++){
+   //console.log(`outer loop $ {i}`);
+  //for(let j=1; j<=3; j++){
+//console.log(j);
+ //}
+//}
+//let i=1;
+//while(i<=5){
+   //console.log(i);
+   //i++;
+//}
+//let i=5;
+//while(i>=1){
+  // console.log(i);
+   //i--;
+//}
+//let i=1;
+//while(i<=20){
+   //console.log(i);
+   //i=i+2;
+//}
+//const favmovie="avatar";
+//let guess=promot("guess my favourite movie");
+//while((guess!=favmovie)&&(guess!="quit")){
+ //  guess=prompt("wrong guess.please try again");
+//}
+//if(guess==favmovie){
+//console.log("congrats!!");
+//}
+//else{
+   //console.log("you quit");
+   //breaks;
+//}
+//let i=1;
+//while(i<=5){
+  // if(i==3){
+     // break;
+   //}
+  // console.log(i);
+  // i++;
+//}
+//console.log("we used break at 3");
+//let fruits=["mango","lichi","banana","apple"];
+//for(i=0;i<=fruits.length; i=i+2){
+//console.log(i,fruits[i]);
+//}
+//let heroes=[
+  // ["spiderman","thor","ironman"],
+  // ["superman","wonderman","flash"]
+//];
+//for(let i=0; i<=heroes.length; i++){
+//console.log(i,heroes[i]);
+//}
+//let student=[["aman",95],["shardha",94.4],["karan",100]];
+//for(let i=0;i<=student.length; i++){
+//for(let j=0;j<student[i].length; j++){
+//console.log(student[i][j]);
+//}
+//}
+//let fruits=["mango","lichi","banana","orange","apple"];
+//for(fruits of fruits){
+  // console.log(fruits);
+//}{
+
+//}
+//for(char of "apnacollege"){
+  // console.log(char);
+//}
+//let heroes=[["superman","batsman","wonderwoman"],["spiderman","ironman","thor"]];
+//for (list of heroes){
+ //for(hero of list){
+    //  console.log(heroes);
+ // }
+// //}
+// let todo = [];
+// let req = prompt("Please enter your request");
+
+// while (true) {
+//     if (req === "quit") {
+//         console.log("Quitting app");
+//         break;
+//     } else if (req === "list") {
+//         console.log("---------");
+//         for (task of todo) {
+//             console.log(task);
+//         }
+//         console.log("---------");
+//     } else if (req === "add") {
+//         let task = prompt("Please enter the task you want to add");
+//         todo.push(task);
+//         console.log("Task added");
+//     }
+
+//     req = prompt("Please enter your request");
+// }
+
+//
+
+//let student={
+   //name:"shardha",
+  // age:23,
+ //  marks:94.4
+//};
+//let students2=["shardha",23,94.4];
+// let student={
+//    name:"shardha",
+//    age:23,
+//    marks:95.6
+
+// };
+// 
+//const student={
+ //  name:"shardha",
+//age:23,
+  // marks:93.3,
+   //city:"delhi"
+
+//}
+//const classinfo={
+//divya:{
+   //grade:"A+",
+   //city:"Delhi"
+//},
+//shradha:{
+//grade:"A",
+//city:"Pune"
+//},
+//karan:{
+  // grade:"o",
+//city:"mumbai"
+//}
+//}
+//const classinfo=[
+//{
+      //name:"divya",
+//grade:"A+",
+      //city:"mumbai"
+   //},
+   //{
+      //name:"shradha",
+      //grade:"A",
+     //city :"Delhi"
+   //},
+   //{
+      //name:"beauty",
+     // grade:"B",
+      //city:"Bhopal"
+   //}
+//];
+// const max=promot("enter the maximum number");
+// const random=math.floor(math.random()*max)+1;
+// let guess=promot("guess the number");
+// while(true){
+//    if(guess=="quit"){
+//       console.log("user quit");
+//       break
+//    }
+//    if(guess==random){
+//       console.log("you are right!congrats! random number was",random);
+//       break;
+//    }
+//    else{
+//       guess=prompt("your guess was wrong.please wrong")
+//    }
+// }
+// function hello(){
+//    console.log("Hello");
+// }
+// hello;
+// hello;
+// hello;
+// function printName(){
+//    console.log("Apna college");
+//    console.log("shardha khapra");
+// }
+// function print1to5(){
+//    for(i=1; i<=5; i++){
+//       // console.log(i);
+//    }
+// }
+// function isAdult(){
+//    let age=18;
+//    if(age<=18){
+//       console.log("adult");
+//    }else{
+//       console.log("Not adult");
+//    }
+// }
+// isAdult();
+// function printpoem(){
+//    console.log("Twinkle twinkle,little star");
+//    console.log("how i wonder what you are");
+//    console.log("like the diamond in the sky");
+// }
+// printpoem();
+// let rand=Math.floor(Math.random()*6)+1;
+// console.log(rand);
+// rolldice();
+// rolldice();
+// rolldice();
+// rolldice();
+// rolldice();
+// rolldice();
+// function printInfo(name,age){
+// console.log(`${name}'s age is ${age}.`);
+// }
+// printInfo("shardha",23);
+// printInfo("rajat",19);
+// function sum(a,b){
+//    console.log(a+b);
+// }
+// sum(1,2);
+// sum(4,5);
+// sum(7,8);
+// function sum(a,b){
+//    return a+b;
+// }
+//  function calcAvg(a,b,c){
+//    let avg=(a+b+c)/3;
+//  console.log(avg);
+//  }
+//  calcAvg();
+// // sum(1,2,3);
+// // sum(4,5,6);
+// // sum(9,8,7);
+// function printTable(n){
+// for(let i=n;i<=n*10;i+=n){
+//    console.log(i);
+// }
+// }
+// calcAvg(2,4,6);
+//  printTable(40);
+// function sum(a,b){
+//    return a+b;
+// }l
+//sum(sum(1,2),3);
+//console.log(sum);
+// function getsum(n){
+//    let sum=0;
+//    for(let i=1; i<=n;i++){
+//       sum+=i;
+//    }
+//    return sum;
+// }
+// let str=["hi","hello","bye","!"];
+// function concat(str){
+//    let result="";
+//    for(let i=0;i<str.length;i++){
+//       result+=str[i];
+//    }return result;
+// }
+// let sum=54;
+// function calsum(a,b){
+//    let sum=a+b;
+//    console.log(sum);
+
+// }
+// calsum(1,2);
+// let age=25;
+// if(age<=18){
+//    let str="adult";
+// console.log(str);
+// }
+// else{
+//    let str="not adult";
+//    console.log(str);
+// }
+//function outerfunc(){
+  // let x=5;
+   //let y=6;
+  // function innerfunc(){
+     // let a=10;
+//console.log(x);
+//console.log(y);
+  // }
+   //innerfunc();
+  // console.log(a);
+//}
+//let greet="hello";//global scope
+//function changeGreet(){
+//   let greet="namaste";//function scope
+  // console.log(greet);
+ //  function innergreet(){
+   //   console.log(greet);//lexial scope
+  // }
+
+//}
+//console.log(greet);
+//changeGreet();
+//let name="divya";
+//let sum=function(a,b){
+  // return a+b;
+//}
+//let hello=function(){
+  // console.log("hello");
+
+//}
+//hello =function(){
+  // console.log("namaste");
+//}
+//function multipleGreet(func,count){
+  // for(let i=1;i<=Greet; i++){
+     // func();
+
+   //}
+//} 
+//let Greet=function(){
+//console.log(Greet);
+//}
+//multipleGreet(Greet,100);
+//function oddEvenfactory(request){
+   //if (request=="odd"){
+      //return function(n){
+      //   console.log(!(n%2==0));
+   //   }
+  // }elseif (request=="even"){                                  
+      //return function(n){
+      //   console.log(n%2==0);
+     // }
+  // }
+   //else {
+     // console.log("wrong request");
+//}
+   
+  // }
+//const calculator={
+  // num:55,
+   //add:function(a,b){
+   //   return a+b;
+  // },
+  // sub:function(a,b){
+//return a-b;
+//   },
+ //  mul:function(a,b){
+     // return a*b;
+   //},
+   //div:function(a,b){
+     // return a/b;
+  // }
+
+//}
+// let canvas=   document.querySelector('canvas')
+
+// let ctx=  canvas.getContext('2d')
+// let cell=50
+// let snekcell=[[0,0]]
+// let direction='right'
+// let gameOver=false
+
+// let foodcell=random()
+
+//  let id=  setInterval(()=>{
+//     draw()
+//     update()
+// },100)
+// document.addEventListener('keydown',function(e){
+//     // console.log(e);
+//     if(e.key==='ArrowUp'){
+//         direction='up'
+       
+//     }
+// else if(e.key==='ArrowDown'){
+//     direction='down'
+   
+// }
+// else if(e.key==='ArrowLeft'){
+//     direction='left'
+   
+// }
+// else{
+//     direction='right'
+   
+
+// }
+
+// })
+
+// function draw(){
+//     if(gameOver){
+//         clearInterval(id)
+//         return;
+//     }
+//     ctx.clearRect(0,0,1200,800)
+//     for(let i of snekcell){
+//         ctx.fillStyle='red'
+
+//         ctx.fillRect(i[0],i[1],cell,cell)
+//     }
+//     ctx.fillStyle='green'
+//     ctx.fillRect(foodcell[0],foodcell[1],cell,cell)
+
+
+// }
+
+// draw()
+
+// function update(){
+//  let headX=   snekcell[snekcell.length-1][0]
+//      let headY=  snekcell[snekcell.length-1][1]
+//      let newX
+//      let newY
+//      if(direction==='up'){
+//         newX=headX
+//         newY=headY-cell
+//         if(newY<0){
+//             gameOver=true
+//         }
+//      }
+//      else if( direction==='left'){
+//         newX=headX-cell
+//         newY=headY
+//         if(newX<0){
+//             gameOver=true
+//         }
+//      }
+//      else if(direction==='down'){
+//         newX=headX
+//         newY=headY+cell
+//         if(newY===800){
+//             gameOver=true
+//         }
+//      }
+//      else{
+//         newX=headX+cell
+//         newY=headY
+//         if(newX===1200){
+//             gameOver=true
+//         }
+//      }
+
+//      snekcell.push([newX,newY])
+
+
+//      if(newX===foodcell[0]   && newY===foodcell[1]){
+//         foodcell=random()
+//      }
+//      else{
+//         snekcell.shift()
+
+//      }
+
+
+
+// }
+
+
+
+// function random(){
+
+// return [  Math.floor( Math.random()*1750/50)*50   ,
+//        Math.floor( Math.random()*750/50)*50 
+// ]
+
+
+// }
+
+
+
+
+
+// // setInterval(()=>{
+// //     console.log('chalt rh');
+// // },10)
+
+
+
+
+// let arr=[1,2,34,45]
+// arr.push(50)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+
+
+
+
+
+// let key='9f6290d6cda9a36a63755fadee71f83d'
+
+// let city='pakistan'
+
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+// .then((data)=>{
+//     return data.json()
+
+// }).then((res)=>{
+//     console.log(res,'hehe');
+
+// })
+const student={
+name:"shardha",
+age:23,
+english:98,
+physics:99,
+math:100,
+get avg(){
+   console.log(this);
+   let avg=(this.english+this.physics+this.math)/3;
+   console.log('${this.name} got avg marks=${avg}');
+}
+}
+function getavg(){
+   console.log(this);
+}
